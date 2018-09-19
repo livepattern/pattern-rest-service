321bcddae48025f8e4b3a9aa6a4072ff066920a0
Manan Mehta <mehtamanan@icloud.com>
2018-09-19 01:36:24 -0700

    add store schemas

--- app/emails/index.js
+++ app/emails/index.js
@@ -3,20 +3,23 @@ let mail = require('@sendgrid/mail')
 
 mail.setApiKey(settings.sendgridAPIKey);
 
-function storeEmail(to, from, order) {
+function storeEmail(to, from, b) {
   from = from ? from : settings.email;
 
+  let _text = `Yo team,
+    
+    A new order has been placed on Live Pattern:
+    
+    Name: ${b.name}
+    Email: ${b.email}
+    Phone: ${b.phone}
+    `;
 
   let message = {
     to: to,
     from: from,
     subject: 'ACTION REQUIRED: New order placed on LivePattern!',
-    text: `Yo,
-    
-    New Order Placed:
-    email: ${order.email}
-    name: ${order.name}
-    `
+    text: _text
   };
 
   return mail.send(message);
