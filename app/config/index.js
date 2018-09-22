ed8505b3908d0c6b78a967caf368c19921dec52f
Manan Mehta <mehtamanan@icloud.com>
2018-09-22 13:16:51 -0700

    change default CORS to true

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   email: process.env.EMAIL,
-  cors: process.env.CORS | false,
+  cors: process.env.CORS | true,
   manager: process.env.MANAGER_EMAIL,
   sendgridAPIKey: process.env.SENDGRID_API_KEY,
 };
